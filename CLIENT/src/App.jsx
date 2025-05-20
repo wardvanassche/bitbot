37f95bd01d9eafbc28783ecab58d346dc0e3bcf6
Ward van Assche <1060834@hr.nl>
2025-05-20 16:02:43 +0200

    new project

---
+++ CLIENT/src/App.jsx
@@ -0,0 +1,113 @@
+import React, {useEffect, useState} from "react";
+import "./app.css"
+import showdown from "showdown";
+import bitcoin from "./images/bitcoin.png"
+
+function App() {
+    const [prompt, setPrompt] = useState('')
+    const [output, setOutput] = useState('')
+    const [history, setHistory] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    // converter for showdown html
+    const converter = new showdown.Converter();
+
+    // Load local stored history and add to history
+    useEffect(() => {
+        const localStoredHistory = JSON.parse(localStorage.getItem("history") || "[]")
+        setHistory(localStoredHistory)
+        console.log("Loaded history from localStorage: ", localStoredHistory)
+    }, []);
+
+
+    const askQuestion = async (e) => {
+        e.preventDefault()
+
+        setLoading(true)
+
+        console.log(history)
+
+        const options = {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify(
+                {
+                    history: history,
+                    prompt: prompt
+                })
+        }
+
+        const response = await fetch("http://localhost:3000/", options)
+
+        if (response.ok) {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder('utf-8')
+            let finalResult = ""
+
+            while (true) {
+                const {value, done} = await reader.read()
+                if (done) break
+
+                const chunk = decoder.decode(value, {stream: true})
+                console.log(chunk)
+
+                finalResult += chunk
+                setOutput(finalResult)
+            }
+
+            // new history takes previous history and adds current prompt and AI message
+            const newHistory = [...history, {human: prompt, ai: finalResult}]
+
+            // Add new history to local storage
+            localStorage.setItem("history", JSON.stringify(newHistory))
+            // Update history
+            setHistory(newHistory)
+
+            setPrompt("");
+            setLoading(false)
+
+        } else {
+            console.error(`Error while fetching data from ${response.url}`)
+            console.error(`HTTP Status: ${response.status} ${response.statusText}`)
+            setLoading(false)
+        }
+    }
+
+    return (
+        <div className={"main"}>
+            <div className="chat-container">
+                <div className="header">
+                    <img src={bitcoin} alt={"bitcoin"}/>
+                    <h1>BitBot</h1>
+                </div>
+                <div className={"output-container"}>
+                    <div id={"output"}>
+                        <p dangerouslySetInnerHTML={{__html: converter.makeHtml(output)}}>
+                        </p>
+                    </div>
+                </div>
+
+                <form onSubmit={askQuestion}>
+                    <input id={"input"}
+                           type={"text"}
+                           value={prompt}
+                           onChange={e => setPrompt(e.target.value)}
+                           placeholder={"Ask anything..."}
+                    />
+                    <button id={"btn"} type={"submit"} disabled={loading}>
+                        {loading ? "Processing..." : "Ask"}
+                    </button>
+                </form>
+            </div>
+            <div className="footer">
+                <p>© {new Date().getFullYear()} BitBot. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+
+export default App
---
+++ SERVER/index.js
@@ -0,0 +1,77 @@
+import {AzureChatOpenAI} from "@langchain/openai"
+import express, {json} from "express"
+import cors from "cors"
+import {AIMessage, HumanMessage, SystemMessage} from "@langchain/core/messages"
+import fetch from "node-fetch";
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const model = new AzureChatOpenAI({
+    temperature: 0.8,
+})
+
+app.get("/", (req, res) => {
+    res.send("Server running!")
+})
+
+app.post("/", async (req, res) => {
+    let prompt = req.body.prompt
+    let history = req.body.history
+
+    console.log("Received prompt:", prompt)
+    console.log("Received history:", history)
+
+
+    // Fetch coinwatch api
+    const fetchCoinWatchData = async () => {
+        const COINWATCH_API_KEY = process.env.COINWATCH_API_KEY;
+
+        const response = await fetch('https://api.livecoinwatch.com/coins/list', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': COINWATCH_API_KEY
+            },
+            body: JSON.stringify({
+                codes: ['BTC', 'ETH'],
+                currency: 'USD',
+                meta: true
+            })
+        });
+
+        return await response.json();
+    };
+
+    const coinWatchData = await fetchCoinWatchData();
+    console.log(coinWatchData);
+
+
+    // Add date and time
+    const date = new Date()
+
+
+    // Empty array messages for history
+    let messages = [
+        new SystemMessage(`You are a Bitcoin and Ethereum expert and assistant. Use this live Bitcoin data to answer the users prompts: ${JSON.stringify(coinWatchData)}. Today's date and time is ${date}. Please use markdown for styling.`)
+    ]
+
+    for (const {human, ai} of history) {
+        messages.push(new HumanMessage(human))
+        messages.push(new AIMessage(ai))
+    }
+
+    messages.push(new HumanMessage(prompt))
+
+    const stream = await model.stream(messages)
+    res.setHeader("Content-Type", "text/plain")
+    for await (const chunk of stream) {
+        console.log(chunk.content)
+        res.write(chunk.content)
+    }
+    res.end()
+})
+
+app.listen(3000, () => console.log(`Server running on http://localhost:3000`))
